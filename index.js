6c1ca74344b92abdb3521e011a1af70bc39a73dd
ssitl <yyy666@yandex.ru>
2016-11-06 19:30:49 +0300

    DB Test

--- index.js
+++ index.js
@@ -6,6 +6,10 @@ app.get("/", function(req, res){
   res.send('test');
 });
 
+app.get("/db", function(req, res){
+  res.send(process.env.MONGO_DB_URL);
+});
+
 var port = process.env.PORT || 3000;
 
 app.listen(port);
