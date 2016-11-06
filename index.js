e8b1853a7e7cfcc9fb6b13f06dbdbcbfa802fba2
ssitl <yyy666@yandex.ru>
2016-11-06 21:05:49 +0300

    One else commit

--- index.js
+++ index.js
@@ -1,13 +1,43 @@
 var express = require('express');
+var mongodb = require('mongodb');
+var morgan = require('morgan');
 
 var app = express();
+var db;
 
-app.get("/", function(req, res){
+function checkDb(req, res, next){
+  if (db){return next();}
+
+  res.send('DB is not ready');
+}
+
+app.use(morgan("dev"));
+
+var router = new express.Router();
+
+app.use("/test", router);
+
+router.get("/", function(req, res){
   res.send('test');
 });
 
-app.get("/db", function(req, res){
-  res.send(process.env.MONGO_DB_URL);
+router.post("/add", checkDb, function(req, res){
+  var list = db.collection("list");
+  var obj = {test : Math.random()};
+
+  list.insert(obj, function(err){
+    if (err) {throw err;}
+	
+    res.status(201).send("Ok");
+  });
+});
+
+router.get("/list", checkDb, function(req, res){
+  db.collection("list").find({}).toArray(function(err, data){
+    if (err) {throw err;}
+ 
+    res.json(data);
+  });  
 });
 
 var port = process.env.PORT || 3000;
@@ -15,3 +45,9 @@ var port = process.env.PORT || 3000;
 app.listen(port);
 
 console.log('Listening on port: ', port);
+
+mongodb.MongoClient.connect(process.env.MONGO_DB_URL, function(err, _db){
+  if (err) {throw err;}
+
+  db = _db;
+});
